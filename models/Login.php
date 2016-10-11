7191bbd5100fe5cfdf83776104f154cf4a498495
dima <dimas.ershov@gmail.com>
2016-10-11 18:14:15 +0300

    работа на формой login

--- models/Login.php
+++ models/Login.php
@@ -17,9 +17,32 @@ class Login extends Model
         return [
 
             [['login','password'],'required'],
-            ['email','email']
+            ['password','validatePassword']
+
 
         ];
     }
 
+        public function validatePassword($attribute,$params)
+        {
+
+            if(!$this->hasErrors()) // если нет ошибок в валидации
+            {
+                $user = $this->getUser(); // получаем пользователя для дальнейшего сравнения пароля
+                if(!$user || !$user->validatePassword($this->password))
+                {
+                    //если мы НЕ нашли в базе такого пользователя
+                    //или введенный пароль и пароль пользователя в базе НЕ равны ТО,
+                    $this->addError($attribute,'Пароль или логин введены неверно');
+                    //добавляем новую ошибку для атрибута password о том что пароль или имейл введены не верно
+                }
+            }
+
+        }
+
+
+    public function getUser()
+    {
+        return User::findOne(['login'=>$this->login]); // а получаем мы его по введенному login
+    }
 }
