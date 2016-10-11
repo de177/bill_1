473c6cd372bb7996d21d11d0d6fbb5c12084cd5a
dima <dimas.ershov@gmail.com>
2016-10-11 10:50:09 +0300

    работа на формой регистриции

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,11 +3,8 @@
 namespace app\controllers;
 
 use Yii;
-use yii\filters\AccessControl;
 use yii\web\Controller;
-use yii\filters\VerbFilter;
-use app\models\LoginForm;
-use app\models\ContactForm;
+use app\models\Signup;
 
 class SiteController extends Controller
 {
@@ -18,6 +15,7 @@ class SiteController extends Controller
 
     public function actionSignup()
     {
-        return $this->render('signup');
+        $model = new Signup();
+        return $this->render('signup',[ 'model' =>$model] );
     }
 }
---
+++ models/Signup.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+
+class Signup extends Model
+
+{
+    public $login;
+    public $password;
+    public $email;
+
+}
