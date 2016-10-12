3a3fefd8c559298755258bb9bef74403c654be97
dima <dimas.ershov@gmail.com>
2016-10-12 16:08:08 +0300

    все удалил

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=db_test',
+    'username' => 'site.bill',
+    'password' => 'Zaq12wsx',
     'charset' => 'utf8',
 ];
