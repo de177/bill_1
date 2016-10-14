8830dec6d247e8df5c7c4430f034daabc5013453
dima <dimas.ershov@gmail.com>
2016-10-14 13:43:25 +0300

    работа с логином

--- models/Login.php
+++ models/Login.php
@@ -14,11 +14,26 @@ class Login extends Model
     public function rules()
     {
         return [
-            [['login','password'],'required']
+            [['login','password'],'required'],
+            ['password', 'validatePassword']
 
         ];
     }
 
+    public function validatePassword($attribute,$params)
+    {
+
+        $user = User::findOne(['login'=>$this->login]);
+
+        if(!$user || $user->password != sha1($this->password)))
+        {
+            //если мы НЕ нашли в базе такого пользователя
+            //или введенный пароль и пароль пользователя в базе НЕ равны ТО,
+            $this->addError($attribute,'Пароль или имейл введены неверно');
+            //добавляем новую ошибку для атрибута password о том что пароль или имейл введены не верно
+        }
+
+    }
 
 }
 
