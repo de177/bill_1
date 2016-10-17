90eca5a4744c776afeb29c39220ad6e572c7b8dd
dima <dimas.ershov@gmail.com>
2016-10-17 12:24:13 +0300

    Начало изучения представлений

--- views/site/index.php
+++ views/site/index.php
@@ -1,1 +1,20 @@
 <h1> Мы на главной странице</h1>
+
+
+<?php
+use yii\bootstrap\DatePicker;
+?>
+
+
+<?= DatePicker::widget([
+    'model' => $model,
+    'attribute' => 'from_date',
+    'language' => 'ru',
+    'clientOptions' => [
+        'dateFormat' => 'yy-mm-dd',
+    ],
+])
+
+?>
+
+
