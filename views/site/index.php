c6d26db02fa4603dfaafb8c3208af151e8936366
dima <dimas.ershov@gmail.com>
2016-10-17 14:38:28 +0300

    Начало изучения представлений

--- views/site/index.php
+++ views/site/index.php
@@ -5,21 +5,22 @@
 
 use yii\widgets\ActiveForm;
 use kartik\field\FieldRange;
-use kartik\date\DatePicker;
+use \kartik\datetime\DateTimePicker
 
 ?>
 
 <?php $form = ActiveForm::begin(); ?>
 
-echo 'Birth Date';
-<?= DatePicker::widget([
-'name' => 'dp_1',
-'type' => DatePicker::TYPE_INPUT,
-'value' => '23-Feb-1982',
-'pluginOptions' => [
-'autoclose'=>true,
-'format' => 'dd-M-yyyy'
-]
+echo 'Event Time';
+<?=
+    DateTimePicker::widget([
+    'name' => 'dp_1',
+    'type' => DateTimePicker::TYPE_INPUT,
+    'value' => '23-Feb-1982 10:10',
+    'pluginOptions' => [
+        'autoclose'=>true,
+        'format' => 'dd-M-yyyy hh:ii'
+    ]
 ]);
 
 ?>
