b4c15d2393dc8fc65996ee347ad434bd0326b521
dima <dimas.ershov@gmail.com>
2016-10-18 15:08:16 +0300

    Начало изучения sidebar

--- views/site/index.php
+++ views/site/index.php
@@ -3,6 +3,8 @@
 use yii\widgets\Menu;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
+use kartik\sidenav\SideNav;
+use yii\helpers\Url;
 ?>
 
 <h1> Мы на главной странице</h1>
@@ -36,53 +38,32 @@ use yii\bootstrap\NavBar;
 
 <?php
 
-    NavBar::begin([
-        'brandLabel' => 'Yii Navbar',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-        'class' => 'navbar-default'
-                    ],
-    ]);
-
-    echo Nav::widget([
-    'options' => [
-        'class' => 'navbar-nav navbar-right'
-    ],
+echo SideNav::widget([
+    'type' => $type,
+    'encodeLabels' => false,
+    'heading' => $heading,
     'items' => [
-        [
-            'label' => 'Главная',
-            'url' => [
-                '#'
-            ]
-        ],
-        [
-            'label' => 'About',
-            'url' => [
-                '#'
-            ]
-        ],
-        [
-            'label' => 'Обратная связь',
-            'url' => [
-                '#'
-            ]
-        ],
-        Yii::$app->user->isGuest ? [
-            'label' => 'Войти',
-            'url' => [
-                '#'
-            ]
-        ] : [
-            'label' => 'Выйти ('.Yii::$app->user->identity->login.')',
-            'url' => [
-                '#'
-            ],
-            'linkOptions' => [
-                'data-method' => 'post'
-            ]
-        ]
+        // Important: you need to specify url as 'controller/action',
+        // not just as 'controller' even if default action is used.
+        ['label' => 'Home', 'icon' => 'home', 'url' => Url::to(['/site/home', 'type'=>$type]), 'active' => ($item == 'home')],
+        ['label' => 'Books', 'icon' => 'book', 'items' => [
+            ['label' => '<span class="pull-right badge">10</span> New Arrivals', 'url' => Url::to(['/site/new-arrivals', 'type'=>$type]), 'active' => ($item == 'new-arrivals')],
+            ['label' => '<span class="pull-right badge">5</span> Most Popular', 'url' => Url::to(['/site/most-popular', 'type'=>$type]), 'active' => ($item == 'most-popular')],
+            ['label' => 'Read Online', 'icon' => 'cloud', 'items' => [
+                ['label' => 'Online 1', 'url' => Url::to(['/site/online-1', 'type'=>$type]), 'active' => ($item == 'online-1')],
+                ['label' => 'Online 2', 'url' => Url::to(['/site/online-2', 'type'=>$type]), 'active' => ($item == 'online-2')]
+            ]],
+        ]],
+        ['label' => '<span class="pull-right badge">3</span> Categories', 'icon' => 'tags', 'items' => [
+            ['label' => 'Fiction', 'url' => Url::to(['/site/fiction', 'type'=>$type]), 'active' => ($item == 'fiction')],
+            ['label' => 'Historical', 'url' => Url::to(['/site/historical', 'type'=>$type]), 'active' => ($item == 'historical')],
+            ['label' => '<span class="pull-right badge">2</span> Announcements', 'icon' => 'bullhorn', 'items' => [
+                ['label' => 'Event 1', 'url' => Url::to(['/site/event-1', 'type'=>$type]), 'active' => ($item == 'event-1')],
+                ['label' => 'Event 2', 'url' => Url::to(['/site/event-2', 'type'=>$type]), 'active' => ($item == 'event-2')]
+            ]],
+        ]],
+        ['label' => 'Profile', 'icon' => 'user', 'url' => Url::to(['/site/profile', 'type'=>$type]), 'active' => ($item == 'profile')],
     ],
 ]);
-NavBar::end();
 ?>
 
