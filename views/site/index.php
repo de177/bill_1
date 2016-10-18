d8095346360cd96613bb1e515c127a1773026880
dima <dimas.ershov@gmail.com>
2016-10-18 15:41:06 +0300

    Начало изучения sidebar

--- views/site/index.php
+++ views/site/index.php
@@ -39,30 +39,30 @@ use yii\helpers\Url;
 <?php
 
      SideNav::widget([
-    'success' => $type,
+    'type' => SideNav::TYPE_SUCCESS,
     'encodeLabels' => false,
-    'Operations' => '$heading',
+    'heading' => '<i class="glyphicon glyphicon-cog"></i> Operations',
     'items' => [
         // Important: you need to specify url as 'controller/action',
         // not just as 'controller' even if default action is used.
-        ['label' => 'Home', 'icon' => 'home', 'url' => Url::to(['/site/home', 'type'=>$type]), 'active' => ($item == 'home')],
+        ['label' => 'Home', 'icon' => 'home', 'url' => Url::to(['/site/home', 'type'=>SideNav::TYPE_SUCCESS]), 'active' => ($item == 'home')],
         ['label' => 'Books', 'icon' => 'book', 'items' => [
-            ['label' => '<span class="pull-right badge">10</span> New Arrivals', 'url' => Url::to(['/site/new-arrivals', 'type'=>$type]), 'active' => ($item == 'new-arrivals')],
-            ['label' => '<span class="pull-right badge">5</span> Most Popular', 'url' => Url::to(['/site/most-popular', 'type'=>$type]), 'active' => ($item == 'most-popular')],
+            ['label' => '<span class="pull-right badge">10</span> New Arrivals', 'url' => Url::to(['/site/new-arrivals', 'type'=>SideNav::TYPE_SUCCESS]), 'active' => ($item == 'new-arrivals')],
+            ['label' => '<span class="pull-right badge">5</span> Most Popular', 'url' => Url::to(['/site/most-popular', 'type'=>SideNav::TYPE_SUCCESS]), 'active' => ($item == 'most-popular')],
             ['label' => 'Read Online', 'icon' => 'cloud', 'items' => [
-                ['label' => 'Online 1', 'url' => Url::to(['/site/online-1', 'type'=>$type]), 'active' => ($item == 'online-1')],
-                ['label' => 'Online 2', 'url' => Url::to(['/site/online-2', 'type'=>$type]), 'active' => ($item == 'online-2')]
+                ['label' => 'Online 1', 'url' => Url::to(['/site/online-1', 'type'=>SideNav::TYPE_SUCCESS]), 'active' => ($item == 'online-1')],
+                ['label' => 'Online 2', 'url' => Url::to(['/site/online-2', 'type'=>SideNav::TYPE_SUCCESS]), 'active' => ($item == 'online-2')]
             ]],
         ]],
         ['label' => '<span class="pull-right badge">3</span> Categories', 'icon' => 'tags', 'items' => [
-            ['label' => 'Fiction', 'url' => Url::to(['/site/fiction', 'type'=>$type]), 'active' => ($item == 'fiction')],
-            ['label' => 'Historical', 'url' => Url::to(['/site/historical', 'type'=>$type]), 'active' => ($item == 'historical')],
+            ['label' => 'Fiction', 'url' => Url::to(['/site/fiction', 'type'=>SideNav::TYPE_SUCCESS]), 'active' => ($item == 'fiction')],
+            ['label' => 'Historical', 'url' => Url::to(['/site/historical', 'type'=>SideNav::TYPE_SUCCESS]), 'active' => ($item == 'historical')],
             ['label' => '<span class="pull-right badge">2</span> Announcements', 'icon' => 'bullhorn', 'items' => [
-                ['label' => 'Event 1', 'url' => Url::to(['/site/event-1', 'type'=>$type]), 'active' => ($item == 'event-1')],
-                ['label' => 'Event 2', 'url' => Url::to(['/site/event-2', 'type'=>$type]), 'active' => ($item == 'event-2')]
+                ['label' => 'Event 1', 'url' => Url::to(['/site/event-1', 'type'=>SideNav::TYPE_SUCCESS]), 'active' => ($item == 'event-1')],
+                ['label' => 'Event 2', 'url' => Url::to(['/site/event-2', 'type'=>SideNav::TYPE_SUCCESS]), 'active' => ($item == 'event-2')]
             ]],
         ]],
-        ['label' => 'Profile', 'icon' => 'user', 'url' => Url::to(['/site/profile', 'type'=>$type]), 'active' => ($item == 'profile')],
+        ['label' => 'Profile', 'icon' => 'user', 'url' => Url::to(['/site/profile', 'type'=>SideNav::TYPE_SUCCESS]), 'active' => ($item == 'profile')],
     ],
 ]);
 ?>
