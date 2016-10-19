4710687c5c5b3769c84cf47b131d0cfa24fb9d6b
dima <dimas.ershov@gmail.com>
2016-10-19 14:08:06 +0300

    Начало изучения sidebar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,7 +37,20 @@ AppAsset::register($this);
 
                 <div class="clearfix"></div>
 
-                <!-- menu prile quick info -->
+                <!-- menu profile quick info -->
+
+                <div class="profile">
+                    <div class="profile_pic">
+                        <img src="http://placehold.it/128x128" alt="..." class="img-circle profile_img">
+                    </div>
+                    <div class="profile_info">
+                        <span>Welcome,</span>
+                        <h2>John Doe</h2>
+                    </div>
+                </div>
+                <!-- /menu profile quick info -->
+
+
 
 
 <div id="navBar" class="col-sm-12">
