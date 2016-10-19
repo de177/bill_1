026f04db7775a6d841a3562fa0cfd4155d992929
dima <dimas.ershov@gmail.com>
2016-10-19 17:41:23 +0300

    Начало изучения sidebar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -105,7 +105,7 @@ AppAsset::register($this);
 
         <!-- page content -->
 
-        <div class="container">
+        <div class="jumbotron">
         <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],]) ?>
             <?= $content ?>
         </div>
