c0fe02a7aa2275e9376eaa4ac7b1b97b2ca5e7cf
dima <dimas.ershov@gmail.com>
2016-10-20 09:50:34 +0300

    Начало изучения sidebar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -21,100 +21,35 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
-<body class="nav-md">
+<body>
     <?php $this->beginBody() ?>
 
-    <div class="container body">
 
-        <div class="col-md-3 left_col">
-
-            <div class="left_col scroll-view">
-
-                <div class="navbar nav_title" style="border: 0;">
-
-                    <a href="/" class="GTEL Admin Interface"><i class="fa fa-paw"></i> <span>Gorizont-Telecom</span></a>
-                </div>
-
-                <div class="clearfix"></div>
 
                 <!-- menu profile quick info -->
 
-                <div class="profile">
 
-                    <div class="profile_info">
-
-                        <span>Добро пожаловать !</span>
-                    </div>
-                </div>
                 <!-- /menu profile quick info -->
 
-                <br />
+
 
                 <!-- sidebar menu -->
-                <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
 
-                    <div class="menu_section">
-
-                        <?php $this->beginContent('@app/views/layouts/left_sidebar.php'); ?>
-
-                        <?php $this->endContent(); ?>
-                    </div>
-                 </div>
                 <!-- /sidebar menu -->
 
-            </div>
-        </div>
 
         <!-- top navigation -->
 
-        <div class="wrap">
-            <?php
-            NavBar::begin([
-                'brandLabel' => 'My Company',
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
-                    Yii::$app->user->isGuest ? (
-                    ['label' => 'Login', 'url' => ['/site/login']]
-                    ) : (
-                        '<li>'
-                        . Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
-                        . Html::submitButton(
-                            'Logout (' . Yii::$app->user->identity->login . ')',
-                            ['class' => 'btn btn-link']
-                        )
-                        . Html::endForm()
-                        . '</li>'
-                    )
-                ],
-            ]);
-            NavBar::end();
-            ?>
 
-        </div>
 
         <!-- /top navigation -->
 
         <!-- page content -->
 
-        <div class="context">
 
-            <?= Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],]) ?>
-
-            <?= $content ?>
-        </div>
 
         <!-- /page content -->
 
-    </div>
 
 
 
