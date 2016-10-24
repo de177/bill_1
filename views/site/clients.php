a5581229ca1a146913c7b34685efbaed24ec9a9a
dima <dimas.ershov@gmail.com>
2016-10-24 17:06:31 +0300

    Начало изучения CRUD

--- views/site/clients.php
+++ views/site/clients.php
@@ -6,7 +6,7 @@
 
     <?php foreach ($arrayInView as $item): ?>
         <p>
-            <a herf ="/site/view/<?= $item ->id?>"> <?php echo $item -> name ?></a>
+            <a href ="/site/view/<?= $item ->id?>"><?php echo $item -> name ?></a>
         </p>
 
 
