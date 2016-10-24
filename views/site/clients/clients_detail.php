d3aeaacd6cabcc44c8fd6968f67057088fdd7751
dima <dimas.ershov@gmail.com>
2016-10-24 17:36:03 +0300

    Начало изучения CRUD

--- views/site/clients/clients_detail.php
+++ views/site/clients/clients_detail.php
@@ -1,6 +1,6 @@
 <h1>Детальная информация о клиенте</h1>
 
-<h3><?= $one -> name ?>&nbsp<?= $one -> name ?></h3>
+<h3><?= $one -> org_form ?>&nbsp<?= $one -> name ?></h3>
 
 <p>
     <?= $one -> email ?>
