291304a7eba0c8d3b1ef82f0e728529dc1d8e09b
dima <dimas.ershov@gmail.com>
2016-10-26 17:32:07 +0300

    Начало изучения CRUD

--- views/clients/create.php
+++ views/clients/create.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Clients */
 
-$this->title = 'Create Clients';
+$this->title = 'Добавить клиента';
 $this->params['breadcrumbs'][] = ['label' => 'Clients', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
