8910af91996193b20dc50aec3c8a4645944cba8c
dima <dimas.ershov@gmail.com>
2016-10-26 15:56:47 +0300

    Начало изучения CRUD

---
+++ views/clients/_form.php
@@ -0,0 +1,49 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Clients */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="clients-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'tariff_id')->textInput() ?>
+
+    <?= $form->field($model, 'service_id')->textInput() ?>
+
+    <?= $form->field($model, 'service_flag')->textInput() ?>
+
+    <?= $form->field($model, 'login')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'org_form')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'inn')->textInput() ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'postal')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'country')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
