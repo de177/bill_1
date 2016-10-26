29899db137b532ea150f9ddc71816ceab194f58f
dima <dimas.ershov@gmail.com>
2016-10-26 15:48:29 +0300

    Начало изучения CRUD

--- views/clients/index.php
+++ views/clients/index.php
@@ -3,6 +3,9 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Clients', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
