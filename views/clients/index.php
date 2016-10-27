56da2687e35866d948099f18536052a4260338fd
dima <dimas.ershov@gmail.com>
2016-10-27 14:15:41 +0300

    Начало изучения CRUD

--- views/clients/index.php
+++ views/clients/index.php
@@ -7,23 +7,6 @@ $this->title = 'Clients';
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
-<!---
-<h1><?php //echo $cl_var1InView; ?></h1>
-
-
-<div>
-
-    <?php //foreach ($arrayInView as $item): ?>
-        <p>
-            <a href ="/site/clients_detail/<?= //$item ->id?>"><?php //echo $item -> name ?></a>
-        </p>
-
-
-    <?php //endforeach; ?>
-
-
-</div>
---->
 
 <div>
     <p>
--- views/site/clients/clients_detail.php
+++ views/site/clients/clients_detail.php
@@ -6,3 +6,19 @@
     <?= $one -> email ?>
 </p>
 
+
+<h1><?php echo $cl_var1InView; ?></h1>
+
+
+<div>
+
+    <?php foreach ($arrayInView as $item): ?>
+        <p>
+            <a href ="/site/clients_detail/<?= $item ->id?>"><?php echo $item -> name ?></a>
+        </p>
+
+
+    <?php endforeach; ?>
+
+
+</div>
